9c7ca647d628c4fcef7df2443639562fbcbdcc94
Robin <robincouwenberg@yahoo.com>
2021-07-20 22:23:59 +0100

    Changed so that it just ads the first 600 characters as the meta description

--- mkdocs_meta_descriptions_plugin/plugin.py
+++ mkdocs_meta_descriptions_plugin/plugin.py
@@ -15,30 +15,33 @@ class MetaDescription(BasePlugin):
     )
 
     def __init__(self):
-        self._headings_pattern = re.compile("<h2 id=\".*\">.*</h2>", flags=re.IGNORECASE)
+        self._headings_pattern = re.compile("<h[2-6] id=\".*\">.*</h[2-6]>", flags=re.IGNORECASE)
         self._pages = []
 
     def _get_first_paragraph_text(self, html):
         # Strip page subsections to improve performance
-        try:
-            html = re.split(self._headings_pattern, html)[1]
-        except IndexError:
-            html = ""
-        pattern2 = re.compile("<h[2-6]", flags=re.IGNORECASE)
-        try:
-            html = re.split(pattern2, html,maxsplit=1)[0]
-        except IndexError:
-            html = ""
+        # try:
+        #     html = re.split(self._headings_pattern, html)[0]
+        # except IndexError:
+        #     html = "Meta Descriptions not working, please report to owner of the website."
+        
+        # pattern2 = re.compile("<h[2-6]", flags=re.IGNORECASE)
+        # try:
+        #     html = re.split(pattern2, html,maxsplit=1)[0]
+        # except IndexError:
+        #     html = "Meta Descriptions not working, please report to owner of the website."
         
         
         # Select first paragraph directly under body
-        first_paragraph = BeautifulSoup(html, "html.parser")#.select_one("p")
+        first_paragraph = BeautifulSoup(html, "html.parser")
         if first_paragraph is not None:
             # Found the first paragraph, return stripped and escaped text
-            return escape(first_paragraph.get_text().strip())
+            noHtml = first_paragraph.get_text().strip()
+            noHtml = noHtml[:600] if len(noHtml) > 600 else noHtml
+            return escape(noHtml)
         else:
             # Didn't find the first paragraph
-            return ""
+            return "Meta Descriptions not working, please report to owner of the website."
 
     def on_page_content(self, html, page, config, files):
         if page.meta.get("description", None):
